0c0ae1c2828665d7754d6a6ff012ab8f87b69715
Pavlik7777 <Pasha20080620@gmail.com>
2025-11-24 23:37:27 +0200

    Добавьте файлы проекта.

---
+++ LAb61/Program.cs
@@ -0,0 +1,7 @@
+﻿//Лабораторная работа №6
+//Функции
+//Средний уровеньВ. Вариант 30
+Console.Write("Введите число в восьмеричной системе: ");
+string vosem = Console.ReadLine();
+int number = Convert.ToInt32(vosem, 8);
+Console.WriteLine($"Число в десятичной системе счисления: {number}");
