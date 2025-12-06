7f24b1422527a98225cfb18374a0c8d7f9e7f53c
Pavlik7777 <Pasha20080620@gmail.com>
2025-12-06 23:02:00 +0200

    Program.cs

--- LAb61/Program.cs
+++ LAb61/Program.cs
@@ -1,7 +1,11 @@
 ﻿//Лабораторная работа №6
 //Функции
 //Средний уровеньВ. Вариант 30
-Console.Write("Введите число в восьмеричной системе: ");
-string vosem = Console.ReadLine();
-int number = Convert.ToInt32(vosem, 8);
-Console.WriteLine($"Число в десятичной системе счисления: {number}");
+void Con()
+{
+    Console.Write("Введите число в восьмеричной системе: ");
+    string vosem = Console.ReadLine();
+    int number = Convert.ToInt32(vosem, 8);
+    Console.WriteLine($"Число в десятичной системе счисления: {number}");
+}
+Con();
